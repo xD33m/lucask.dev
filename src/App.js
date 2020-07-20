5bea6ca0dcd6ad1fc58c0c0b09d67651297bd072
Lucas <lucaskeller98@gmail.com>
2020-07-20 16:45:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
+import UnderConstruction from 'react-under-construction';
+import 'react-under-construction/build/css/index.css';
 import './App.css';
+import chart from './assets/chart.svg';
+import cat from './assets/cat.svg';
+import space from './assets/space.jpg';
+import time from './assets/time.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UnderConstruction
+      background={{
+        image: space,
+        textColor: '#fff',
+        overlay: {
+          color: '#000',
+          opacity: '.7',
+        },
+      }}
+      logo={{
+        src: cat,
+        alt: 'watch',
+      }}
+      title={{
+        text: 'Lucas Keller',
+      }}
+      description={{
+        text: 'This is just a placeholder :)',
+      }}
+      links={[
+        // {
+        //   url: "https://www.facebook.com/",
+        //   image: "https://image.flaticon.com/icons/svg/220/220200.svg",
+        // },
+        // {
+        //   url: "https://www.twitter.com/",
+        //   image: "https://image.flaticon.com/icons/svg/145/145812.svg",
+        // },
+        {
+          url: 'https://xd33m.github.io/',
+          image: time,
+        },
+        {
+          url: 'https:analytics.lucask.dev',
+          image: chart,
+        },
+      ]}
+    />
   );
 }
 
